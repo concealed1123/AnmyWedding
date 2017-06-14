a40cf480b5559030a909459c5fd4f27aef9e055a
concealed1123 <concealed1123@gmail.com>
2017-06-14 23:20:44 +0800

    新增作品集 新增帳號登入 新增ｔａｂ

---
+++ app/src/main/java/com/org/iii/mywedding/ActMain.java
@@ -0,0 +1,25 @@
+package com.org.iii.mywedding;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ActMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actmain);
+
+        InitialComplement();
+
+    }
+
+    private void InitialComplement() {
+
+    }
+
+}
