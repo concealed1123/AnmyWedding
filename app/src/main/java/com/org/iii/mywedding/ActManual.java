49be0c20e5ff0773fd6f162c940175f1b1bb698c
Chun-Hsiang-Huang <lbjmj0223@gmail.com>
2017-06-16 15:32:34 +0800

    Manual UI Design

---
+++ app/src/main/java/com/org/iii/mywedding/ActManual.java
@@ -0,0 +1,13 @@
+package com.org.iii.mywedding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActManual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actmanual);
+    }
+}
